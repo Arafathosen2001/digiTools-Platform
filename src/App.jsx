7120029aa7375d12af1c9a558eea30b1a586c937
Arafathosen2001 <aarafat985@gmail.com>
2026-03-30 09:14:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+// import Hero from './Component/Hero/Hero'
+// import NavBar from './Component/Nav/NavBar'
+// import Rating from './Component/Rating/Rating'
+
+function App() {
+
+  return (
+    <>
+      <h1>DigiTools</h1>
+    </>
+  )
+}
+
+export default App
